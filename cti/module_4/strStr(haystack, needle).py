d8d954335034264c809a74629fca46c713e4f1a0
George Ashworth <gashwor1@mail.ccsf.edu>
2020-06-28 23:31:58 -0700

    Completed string within a string function.

--- cti/module_4/strStr(haystack, needle).py
+++ cti/module_4/strStr(haystack, needle).py
@@ -1,2 +1,21 @@
+'''
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+'''
+
+
 def strStr(haystack, needle):
-  pass
+    for m in range(len(haystack)):
+        if haystack[m] == needle[0]:
+            n = m
+            match = True
+            for letter in needle:
+                if haystack[n] != letter:
+                    match = False
+                n += 1
+            if match:
+                print(m)
+                return m
+    print(-1)
+    return -1
