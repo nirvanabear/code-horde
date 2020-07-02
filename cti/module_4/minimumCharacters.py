13e7cb7061d1466c28b168784ca8628ecd521eca
George Ashworth <gashwor1@mail.ccsf.edu>
2020-07-01 23:07:34 -0700

    Beginning palandrome algorithm

---
+++ cti/module_4/minimumCharacters.py
@@ -0,0 +1,18 @@
+'''
+Minimum Characters required to make a String Palindromic
+
+You are given a string. The only operation allowed is to insert characters in the beginning of the string. Return the number of characters that are needed to be inserted to make the string a palindrome string
+
+
+Examples:
+Input: ABC
+Output: 2
+Input: AACECAAAA
+Output: 2
+
+'''
+
+
+def minimumCharacters():
+
+
