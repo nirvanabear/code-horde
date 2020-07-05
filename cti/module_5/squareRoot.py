4aca3733c820146d8dcb17873ac3f83ceb854ecc
George Ashworth <gashwor1@mail.ccsf.edu>
2020-07-05 12:40:30 -0700

    Completed squareRoot function.

--- cti/module_5/squareRoot.py
+++ cti/module_5/squareRoot.py
@@ -22,7 +22,15 @@ Explanation: The square root of 8 is 2.82842..., and since
              the decimal part is truncated, 2 is returned.
 
 
-
+** pseudocode **
+counter = 1
+found = false
+while not found:
+    if counter^2 > target:
+        found = true
+    else:
+        counter++
+return counter - 1
 
 
 
@@ -31,5 +39,33 @@ Explanation: The square root of 8 is 2.82842..., and since
 
 
 def squareRoot(num):
+    counter = 1
+    found = False
+    length = 0
+    for digit in str(num):
+        length += 1
+    # print(length)
+    if length > 1:
+        # denominator = (length - 1) // 2       ## EDIT: removed /
+        denominator = (length - 1) / 2
+        counter = int(10 ** denominator)
+    # print(counter)
+    while not found:
+        if counter**2 > num:
+            found = True
+            counter -= 1
+        else:
+            counter += 1
+        # print(counter)
+    return counter
+
+replit = 1000000000000000
+print(squareRoot(replit))
 
 
+
+# def squareRoot(num):
+#     for i in range(10000000000):
+#         if i**2 > num:
+#             i -= 1
+#             return i
