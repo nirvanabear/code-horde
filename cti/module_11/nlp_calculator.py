3056ceff517189432da5fe351d6f362e0200b2c0
George Ashworth <gashwor1@mail.ccsf.edu>
2020-08-13 22:11:17 -0700

    Completed nlp_calculator.

--- cti/module_11/nlp_calculator.py
+++ cti/module_11/nlp_calculator.py
@@ -32,6 +32,22 @@ input operands as arguments to the function
 convert return from function call into words
 
 
+** repl.it unit test **
+
+Traceback (most recent call last):
+  File "/home/runner/unit_tests.py", line 119, in test_division
+    self.assertEquals(nlp_calculator("divide one-hundred by ten"), "ten")
+  File "/home/runner/unit_tests.py", line 34, in nlp_calculator
+    solution = operator(operands[0], operands[1])
+IndexError: list index out of range
+
+Traceback (most recent call last):
+  File "/home/runner/unit_tests.py", line 106, in test_subtraction_with_negative_numbers
+    self.assertEquals(nlp_calculator("subtract ten from five"), "negative five")
+AssertionError: 'negative ' != 'negative five'
+- negative 
++ negative five
+?          ++++
 
 
 '''
@@ -46,23 +62,45 @@ def nlp_calculator(statement):
 
   sentence = re.split(r'\W*\s+', statement)
   operands = []
-  for index in range(len(sentence)):
+  index = 0
+  while index < len(sentence):
+    print("for loop: " + str(index))
     if sentence[index] in translator:
       operator = translator[sentence[index]]
+      print(operator)
     elif sentence[index] in all_together:
       if index < len(sentence) - 1 and sentence[index + 1] in all_together:
         combined = sentence[index] + " " + sentence[index + 1]
         if combined in all_together:
           operands.append(all_together[combined])
           index += 1
+          print("index jump: " + str(index))
       else:
         operands.append(all_together[sentence[index]])
+    index += 1
+  if operator == subtract and "from" in sentence:
+    operands.reverse()
+
   print(operands)
-  print(operator(operands[0], operands[1]))
+  solution = operator(operands[0], operands[1])
+  print(solution)
 
+  solution_string = ""
+  if solution < 0:
+    solution_string = "negative "
 
-  
-  return ""
+  found = False
+  iterator = 0
+  word_nums = list(all_together.keys())
+
+  while not found and iterator < len(all_together):
+    if all_together[word_nums[iterator]] == abs(solution):
+      solution_string += word_nums[iterator]
+      found = True
+    else:
+      iterator += 1
+
+  return solution_string
   
   
   
@@ -102,22 +140,16 @@ translator = {
   "over": divide
 }
 
-the_hundreds = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine', 'thirty', 'thirty-one', 'thirty-two', 'thirty-three', 'thirty-four', 'thirty-five', 'thirty-six', 'thirty-seven', 'thirty-eight', 'thirty-nine', 'forty', 'forty-one', 'forty-two', 'forty-three', 'forty-four', 'forty-five', 'forty-six', 'forty-seven', 'forty-eight', 'forty-nine', 'fifty', 'fifty-one', 'fifty-two', 'fifty-three', 'fifty-four', 'fifty-five', 'fifty-six', 'fifty-seven', 'fifty-eight', 'fifty-nine', 'sixty', 'sixty-one', 'sixty-two', 'sixty-three', 'sixty-four', 'sixty-five', 'sixty-six', 'sixty-seven', 'sixty-eight', 'sixty-nine', 'seventy', 'seventy-one', 'seventy-two', 'seventy-three', 'seventy-four', 'seventy-five', 'seventy-six', 'seventy-seven', 'seventy-eight', 'seventy-nine', 'eighty', 'eighty-one', 'eighty-two', 'eighty-three', 'eighty-four', 'eighty-five', 'eighty-six', 'eighty-seven', 'eighty-eight', 'eighty-nine', 'ninety', 'ninety-one', 'ninety-two', 'ninety-three', 'ninety-four', 'ninety-five', 'ninety-six', 'ninety-seven', 'ninety-eight', 'ninety-nine', 'one hundred']
+the_hundreds = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine', 'thirty', 'thirty-one', 'thirty-two', 'thirty-three', 'thirty-four', 'thirty-five', 'thirty-six', 'thirty-seven', 'thirty-eight', 'thirty-nine', 'forty', 'forty-one', 'forty-two', 'forty-three', 'forty-four', 'forty-five', 'forty-six', 'forty-seven', 'forty-eight', 'forty-nine', 'fifty', 'fifty-one', 'fifty-two', 'fifty-three', 'fifty-four', 'fifty-five', 'fifty-six', 'fifty-seven', 'fifty-eight', 'fifty-nine', 'sixty', 'sixty-one', 'sixty-two', 'sixty-three', 'sixty-four', 'sixty-five', 'sixty-six', 'sixty-seven', 'sixty-eight', 'sixty-nine', 'seventy', 'seventy-one', 'seventy-two', 'seventy-three', 'seventy-four', 'seventy-five', 'seventy-six', 'seventy-seven', 'seventy-eight', 'seventy-nine', 'eighty', 'eighty-one', 'eighty-two', 'eighty-three', 'eighty-four', 'eighty-five', 'eighty-six', 'eighty-seven', 'eighty-eight', 'eighty-nine', 'ninety', 'ninety-one', 'ninety-two', 'ninety-three', 'ninety-four', 'ninety-five', 'ninety-six', 'ninety-seven', 'ninety-eight', 'ninety-nine', 'one-hundred']
 
 
 
 if __name__ == "__main__":
-  phrase = "subtract two and three"
-  nlp_calculator(phrase)
+  phrase = "subtract twenty six from three"
+  phrase2 = "twenty six minus three"
+  phrase3 = "divide one-hundred by ten"
+  phrase4 = "subtract ten from five"
+  print(nlp_calculator(phrase4))
 
 
-# dashless = [re.sub('-', ' ', element) for element in the_hundreds]
-# merged = [re.sub(' ', '', item) for item in dashless]
 
-# dashed = {}
-# undashed = {}
-# one_word = {}
-# for r in range(101):
-#   dashed[the_hundreds[r]] = r
-#   undashed[dashless[r]] = r
-#   one_word[merged[r]] = r
