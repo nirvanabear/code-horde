885f8cfc033f24d5c5a55b6bea92fb6212dad8bd
George Ashworth <gashwor1@mail.ccsf.edu>
2022-08-02 21:04:02 -0700

    Naive implementation @ O(n^2)

---
+++ cti/module_3/merge_intervals_v2.py
@@ -0,0 +1,83 @@
+'''
+
+
+Understand: 	Analyze the problem statement
+    if pairs overlap, rewrite them as a single interval with the high and low as endpoints
+Match: 		    Recall problems you have seen, determine similarities
+    item is in collection
+Plan: 			Perform the steps, write them out, describe the process
+    Example:
+        Given [1,3],[2,6],[8,10],[5,7],[15,18],
+        return [1,7],[8,10],[15,18].
+
+
+    Naive Pseudocode: O(n^2)
+        # create target variable, set to first item
+        create counter, set to length of list
+        while counter is not past end of list of pairs
+
+
+            # save interval to list location
+            compare element to next using indices
+                merge if needed (rewrite target to new value)
+                or delete next if enpoints are within endpoints
+                decrement counter
+            change comparison object during loop (copy item)
+        return new list
+
+    Sort then use a stack Pseudocode
+
+    Sort then merge in-place Pseudocode
+
+
+
+Implement: 	    Translate your plan to code
+Review: 		Make sure your code works, handle corner cases
+Evaluate: 		Determine time & space efficiency of solution
+
+
+Given [1,3],[2,6],[8,10],[5,7],[15,18],
+
+return [1,7],[8,10],[15,18].
+
+
+
+'''
+
+def naive_merge_pairs(pairs):
+    # print("start")
+    i = 0
+    while i < len(pairs):
+        j = i + 1
+        while j < len(pairs):
+            # print(str(pairs[i]) + " vs " + str(pairs[j]))
+            if pairs[i][1] > pairs[j][0]:
+                if pairs[i][0] > pairs[j][0] and pairs[i][1] < pairs[j][1]:
+                    pairs[i] = pairs[j]
+                    # print("op1")
+                    del pairs[j]
+                elif pairs[i][0] < pairs[j][0] and pairs[i][1] < pairs[j][1]:
+                    pairs[i] = [pairs[i][0], pairs[j][1]]
+                    # print("op2")
+                    del pairs[j]
+            if pairs[i][0] < pairs[j][1]:
+                if pairs[i][0] < pairs[j][0] and pairs[i][1] > pairs[j][1]: 
+                    del pairs[j]
+                    # print("op3")
+                elif pairs[i][0] > pairs[j][0] and pairs[i][1] > pairs[j][1]:
+                    pairs[i] = [pairs[j][0], pairs[i][1]]
+                    del pairs[j]
+                    # print("op4")    
+            # print(pairs)
+            j += 1
+        i +=1
+    return pairs
+
+
+    
+
+
+if __name__ == "__main__":
+    print(naive_merge_pairs([[1,3],[2,5],[2,4],[7,9],[6,8]]))
+    print(naive_merge_pairs([[1,3],[2,6],[8,10],[5,7],[15,18]]))
+
