dce63ee906e1d84ea8d9ae9eb426d4512c22cec4
George Ashworth <gashwor1@mail.ccsf.edu>
2022-08-17 23:29:51 -0700

    Added functions for user input and randomizing.

--- c-reboot/a2_blackjack.cpp
+++ c-reboot/a2_blackjack.cpp
@@ -4,7 +4,6 @@
 #include <ios>
 #include <limits>
 #include <string>
-#include <sstream>
 
 using namespace std;
 
@@ -47,23 +46,96 @@ Review: 		Make sure your code works, handle corner cases
 Evaluate: 		Determine time & space efficiency of solution
 */
 
-const int LENGTH = 11;
+//Function prototype
+int dealer();
+void inputCheck(char*);
 
-int main(){
+const int LENGTH = 11;
+const int MAXVALUE = 10;
+const int MINVALUE = 1;
+
+int main() {
+
 
     char seed[LENGTH] = "";    // Cannot be single quote
+    // char* seed = nullptr;    // Same thing, just dynamic allocation
+    // seed = new char[LENGTH];
 
     printf("Input integer for seeded randomizer or press enter for non-deterministic randomizer (10-digit max): ");
     
+    inputCheck(seed);  
+
+    if (strcmp(seed, "") == 0)
+        cout << "Empty!" << endl;
+
+    printf("String: %s \n", seed);
+
+    unsigned int seedling;
+
+    // No null char, only strings
+    if (strcmp(seed, "") == 0) {
+        seedling = strtoul(seed, NULL, LENGTH);
+        srand(seedling);    
+    }      
+    else
+        srand(time(0));
+
+    printf("Integer: %u: \n", seedling);
+    // cout << seedling << endl;
+    cout << rand() << endl;
+
+
+    int y;
+
+    // Limits the range of the random number.
+    while (0) {
+        cin.get();
+        // y = (rand() % (MAXVALUE - MINVALUE + 1)) + MINVALUE;
+        y = dealer();
+        cout << y;
+    }
+
+    
+    int total = 0;
+    int currentCard;
+    int previousCard;
+
+    currentCard = dealer();
+    previousCard = currentCard;
+    currentCard = dealer();
+    total = currentCard + previousCard;
+    
+    printf("First cards: %d, %d\n", currentCard, previousCard);
+    printf("Total: %d\n", total);
+
+    // do {
+    //     printf("Do you want another card? (y/n): ");
+
+    // }
+    // while()
+
+    // delete [] seed;  // Also needed for dynamic alloca
+	// seed = nullptr;
+
+    return 0;
+
+}
+
+
+
+
+
+void inputCheck(char* seed) {
+
+    // char* seed[LENGTH];
     for (;;){
 
-        cin.get(seed, LENGTH, '\n'); // Leaves \n in buffer
+        cin.get(seed, LENGTH, '\n');//Leaves \n in buffer for ignore
         cin.clear();  // Clears error flags
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // needs \n
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
         
         int checkInt = 1;
         for (int i = 0; i < LENGTH; i++) {
-            // cout << seed[i];
             if (!(isdigit(seed[i]) || seed[i] == '\0')) {
                 printf("%c is not an integer. ", seed[i]);
                 checkInt = 0;
@@ -75,57 +147,14 @@ int main(){
         else {
             cout << "Please input a valid positive integer: ";
             for (int j=0; j<LENGTH; j++) {
-                seed[j] = '\0';  // Double quotes are strings not char
+                seed[j] = '\0';  // Double quotes are strings, not char
             }
         }   
     }
-
-    cout << "Out of loop!" << endl;
-
-    if (strcmp(seed, "") == 0)
-        cout << "Empty!" << endl;
-
-    printf("Integer: %s \n", seed);
-   
-    // if (seed >= 0)
-    //     srand(seed);
-    // else
-    //     srand(time(0));
-
-    // cout << rand() << endl;
-    // cout << time(0) << endl;
-    // cout << time(nullptr) << endl;
-
-    // // Limits the range of the random number.
-    // // y = (rand() % (maxValue - minValue + 1)) + minValue;
-
-    string input1 = "";
-    // string input2 = "";
-    // string input3 = "";
-    // char input4[21];
-    // // char single = "";
+}
 
 
 
-
-    // cin >> input4;
-    // input2 = cin.get();
-    // input3 = cin.get();
-
-    stringstream myStream(input1);
-
-    // if (input4 == "")
-    //     cout << "Empty!" << endl;
-
-    
-    // cout << "input4:" << input4 << ":" << endl;
-    // cout << "input1:" << input1 << ":" << endl;
-    // cout << "input2:" << input2 << ":" << endl;
-    // cout << "input3:" << input3 << ":" << endl;
-    // cout << "Done." << endl;
-
-    // cin >> seed;
-    // cout << seed;
-
-    return 0;
+int dealer() {
+    return (rand() % (MAXVALUE - MINVALUE + 1)) + MINVALUE;
 }
