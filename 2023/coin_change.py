46bb4fc36ff2240f86a2fbab696d0e9258ff2daa
George Ashworth <gashwor1@mail.ccsf.edu>
2023-04-09 00:46:23 -0700

    coin_change with production finish.

--- 2023/coin_change.py
+++ 2023/coin_change.py
@@ -36,14 +36,6 @@ Total Methods: 6
 # 5 5 2 2 
 # ...
 
-# sort list
-
-# list of stump lists
-# for num in coinlist:
-    # create blank stump_list
-    # while sum of list <= total_amt
-        # append 
-
 
 # reverse sort list        # [5,2,1]
 # create list_of_addlists
@@ -55,26 +47,6 @@ Total Methods: 6
             # append each
         # if sum of addlist is equal to target_sum:
             # append addlist to list_of_addlists
-
-
-
-# create list of lists accumulator
-# for i in range(len(coinlist[i])):
-    # create a combolist
-    # while sum of combolist < total amount
-
-        # append coinlist[i]
-        # 
-
-
-        #     append another coinlist[i]
-        #     sum the list
-        #     if equal
-
-
-        # for j in coinlist:
-        #     create a jlist
-        #     append j -> i,j
         
 
 '''
@@ -98,54 +70,40 @@ def fn(checklist, listoflists, start)
 
 '''
 
-# c = [1, 2, 5]
 
-# coinlist = reversed(coins)
-# listoflists = []
-# checklist = []
-# start = 0
-# target = 7
 def coin_recurse(coinlist, checklist, listoflists, target, start):
-    for i in range(start, len(coinlist)):
-        print("***********************")
-        print(f"for: {coinlist[i]}")  
-        print(f"starting checklist: {checklist}") 
-        print(f"start index: {start}")         
+    '''Recursively explores permutations of coinlist that have sums
+    which equal the target, then stores matching permutations'''
+    for i in range(start, len(coinlist)):        
         checklist.append(coinlist[i])            
         total = sum(checklist)
-        print(f"checklist sum: {total}")
         if total == target:
-            print(f"target checklist: {checklist}")
-            print(f"target total: {total}")
             addlist = checklist
             listoflists.append(checklist[:])
-            print(f"popped listoflists: {listoflists}")
             checklist.pop()
         elif total > target:
             checklist.pop()
-            print(f"checklist popped: {checklist}")
         elif total < target:
             start = i
-            print(f"call fn")
             coin_recurse(coinlist, checklist, listoflists, target, start)
-            print("function return")
             checklist.pop()
-    # print(f"list of lists: {listoflists}")
-    print("End loop")
-    # return listoflists
 
-
-if __name__ == "__main__":
-    coinlist = [1, 2, 5]
+def coin_change(coinlist, total):
+    '''Wrapper which formats data, initializes objects to be
+    used in permutation recursion, then calls recursive function.'''
     coinlist.reverse()
-    print(f"coinlist: {coinlist}")
     listoflists = []
     checklist = []
     start = 0
-    target = 7
-    print(coin_recurse(coinlist, checklist, listoflists,target, start))
-    print(f"final list of lists: {listoflists}")
+    coin_recurse(coinlist, checklist, listoflists,total, start)
+    return listoflists
+    
+# O(n) == n^n
 
+if __name__ == "__main__":
+    coinlist = [1, 2, 5]
+    total = 7
+    output = coin_change(coinlist, total)
+    print(output)
+    print(len(output))
 
-        # print(f"total: {total}")  
-        # print(f"checklist: {checklist}")  
